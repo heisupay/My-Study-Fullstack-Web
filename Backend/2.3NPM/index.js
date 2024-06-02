8efcab5dee3023909b98b5be80a4c9a68a37c8ce
HeiSupay <sosmedsupay@gmail.com>
2024-06-02 13:29:40 +0700

    QR-Code Challenge

---
+++ Backend/2.3NPM/index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+// var generateName = require("sillyname");
+// var sillyname = generateName();
+
+// console.log("My name is ${sillyName}.");
+
+import superheroes from "superheroes";
+const name = superheroes.random();
+console.log("I am $ {name}!");
