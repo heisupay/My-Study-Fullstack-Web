9244637c90ce68bc47d1d1933847fa51a3b7bc15
HeiSupay <sosmedsupay@gmail.com>
2024-08-04 08:07:32 +0700

    My study react

---
+++ Front End/279-intro-to-jsx-start/279-intro-to-jsx/src/index.jsx
@@ -0,0 +1,24 @@
+/** @format */
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm run dev
+// to launch your react project in your browser
+import React from "react";
+import reactdom from "react-dom";
+
+var react = require("react");
+var reactdom = require(<h1>Hello Supay</h1>, document.getElementById("root"));
+
+// reactdom.render.createElement("h1");
+// h1.innerHTML = "Hello SUpay!";
+// document.getElementById("root").appendChild(h1);
+
+ReactDOM.render(
+  <div>
+    <h1> Hello Supay!</h1>
+    <p>This os a paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
