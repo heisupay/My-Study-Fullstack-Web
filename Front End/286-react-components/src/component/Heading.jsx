56d3538030c972a52e8f154971795bff45b3f8a7
HeiSupay <sosmedsupay@gmail.com>
2024-08-07 20:31:43 +0700

    Playlist React
    Playlist React
    - React Styling Practice
    - React Components
    - React Components Practice
    - Es6 Import Export & Modules
    - Es6 Import Export Practice

---
+++ Front End/286-react-components/src/component/Heading.jsx
@@ -0,0 +1,8 @@
+/** @format */
+
+import React from "react";
+function Heading() {
+  return <h1>My Favorite Foods</h1>;
+}
+
+export default Heading;
---
+++ Front End/286-react-components/src/component/List.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Noodles</li>
+      <li>Soup</li>
+      <li>Chick</li>
+    </ul>
+  );
+}
+
+export default List;
